e12454051be6c457ccaad2abedea969647a9af50
ead <ead@academico.local>
2025-02-17 21:52:45 -0300

    17_02

--- SQL/trigger.sql
+++ SQL/trigger.sql
@@ -1,7 +1,24 @@
-DELIMITER $$
+/*DELIMITER $$
 
 create trigger ESTOQUE
-after update on produto
+after insert on pedido
 for each row
 begin
-if qtdEstoque
+UPDATE produto SET qtdEstoque=qtdEstoque - new.qtdPedido WHERE idCardapio=NEW.idCardapio
+END
+*/
+
+CREATE OR REPLACE FUNCTION atualizar_estoque()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE produto
+    SET qtdEstoque = qtdEstoque - NEW.qtdPedido
+    WHERE idCardapio = NEW.idCardapio;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER estoque
+AFTER INSERT ON pedido
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_estoque();
